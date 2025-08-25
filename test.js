6224b68c79d9c3a952a5be45198959a4dff2448b
Nguyen Hoang Ky <nhktmdzhg@gmail.com>
2025-08-25 12:00:46 +0700

    Revert "Merge remote-tracking branch 'origin/StorageAndState' into StorageAndState"
    This reverts commit 4557cc7dadf18a3195b8d24813c1c672a3b250a8, reversing
    changes made to 73485caf3a1cc62984cd3ae8059ff31be1bc67f0.

--- test.js
+++
@@ -1,6 +0,0 @@
-// import crypt from './crypto-helper.js';
-// crypt.deriveKeyPBKDF2("masterPassword", new TextEncoder().encode("mysalt"))
-//     .then(key => {
-//         console.log(key);
-//     })
-//     .catch(err => console.error(err));
